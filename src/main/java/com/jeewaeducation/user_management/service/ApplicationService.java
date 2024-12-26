b6ce9851259d79b0cad224ef394fcb55cbf0aa93
wishwajith13 <151111080+wishwajith13@users.noreply.github.com>
2024-12-26 20:51:05 +0530

    developed delete and get application

--- src/main/java/com/jeewaeducation/user_management/service/ApplicationService.java
+++ src/main/java/com/jeewaeducation/user_management/service/ApplicationService.java
@@ -1,8 +1,13 @@
 package com.jeewaeducation.user_management.service;
 
+import com.jeewaeducation.user_management.dto.application.ApplicationGetDTO;
 import com.jeewaeducation.user_management.dto.application.ApplicationSaveDTO;
 
 public interface ApplicationService {
 
     String saveApplication(ApplicationSaveDTO applicationSaveDTO);
+
+    String deleteApplication(int applicationId);
+
+    ApplicationGetDTO getApplication(int applicationId);
 }
